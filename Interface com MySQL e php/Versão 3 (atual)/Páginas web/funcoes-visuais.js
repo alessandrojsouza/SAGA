0f5a6554b3e27aa6b35c73eef1cd5dd39ae64a94
K4PP5 <kaelparaguassu@hotmail.com>
2021-10-28 16:22:43 -0300

    versão 3 da aplicação web

---
+++ Interface com MySQL e php/Versão 3 (atual)/Páginas web/funcoes-visuais.js
@@ -0,0 +1,215 @@
+$(document).ready(function(){
+    $.ajaxSetup({ cache: false });
+
+    realTime();
+
+    $("#eletrovalvula_bomba").click(function () { controleEletrovalvulaSubida() });
+    $("#botaoValSub").click(function () { controleEletrovalvulaSubida() });
+
+    $("#eletrovalvula_dreno").click(function () { controleEletrovalvulaDescida() });
+    $("#botaoValDes").click(function () { controleEletrovalvulaDescida() });
+
+    $("#bomba").click(function () { controlarBomba() });
+    $("#botaoBomba").click(function () { controlarBomba() });
+
+
+});
+
+//VALORES DOS RECIPIENTES
+capacidadeCisterna = 0;
+nivelCisterna = 0;
+porcentagemCisterna = 0;
+
+capacidadePoço = 0;
+nivelPoço = 50;
+porcentagemPoço = 100;
+
+function controleEletrovalvulaSubida() {
+    valorAtual = $("#eletrovalvula_bomba").attr("value");
+    valorBomba = $("#bomba").attr("value");
+
+    if (valorAtual == "0") {  //Se eletroválvula estiver desligada, a chamada da função irá enviar requisição pra placa para ligar, mudar a cor da luz e atualizar estado
+        $("#luz1").attr("fill", "#00FF21")
+        $("#eletrovalvula_bomba").attr("value", "1");
+        $("#botaoValSub").removeClass('btn-danger').addClass('btn-success');
+        $("#botaoValSub").text("Aberta");
+
+        $.get("http://192.168.0.109/LED1=ON");
+    
+    } else {   //Se eletroválvula estiver ligada, a chamada da função irá enviar requisição pra placa para desligar, mudar a cor da luz e atualizar estado
+        if (valorBomba == "0") {
+            $("#luz1").attr("fill", "red")
+            $("#eletrovalvula_bomba").attr("value", "0");
+            $("#botaoValSub").removeClass('btn-success').addClass('btn-danger');
+            $("#botaoValSub").text("Fechada");
+
+            $.get("http://192.168.0.109/LED1=OFF");
+
+        } else {    //Aviso para evitar forçar a bomba
+            window.alert("Desligue a bomba antes de fechar a válvula")
+        } 
+    }
+}
+
+function controleEletrovalvulaDescida() {
+    valorAtual = $("#eletrovalvula_dreno").attr("value");  //Valor atual da eletroválvula            
+
+    if (valorAtual == "0") {    ////Se eletroválvula estiver desligada, a chamada da função irá enviar requisição pra placa para ligar, mudar a cor da luz e atualizar estado
+        $("#luz2").attr("fill", "#00FF21")
+        $("#eletrovalvula_dreno").attr("value", "1");
+        $("#botaoValDes").removeClass('btn-danger').addClass('btn-success');
+        $("#botaoValDes").text("Aberta");
+        $("#aguaDreno").attr("stroke","#5B9BA2");
+
+        $.get("http://192.168.0.109/LED2=ON");
+        
+    } else {    //Se eletroválvula estiver ligada, a chamada da função irá enviar requisição pra placa para desligar, mudar a cor da luz e atualizar estado
+        $("#luz2").attr("fill", "red")
+        $("#eletrovalvula_dreno").attr("value", "0");
+        $("#botaoValDes").removeClass('btn-success').addClass('btn-danger');
+        $("#botaoValDes").text("Fechada");
+        $("#aguaDreno").attr("stroke","#7f3f00");
+
+        $.get("http://192.168.0.109/LED2=OFF");
+    }
+}
+
+function controlarBomba() {
+    valorAtual = $("#bomba").attr("value"); //Estado atual da bomba
+    valorValvulaSubida = $("#eletrovalvula_bomba").attr("value");     //Estado atual da eletroválvula de subida
+
+    if (valorAtual == "0") {     //Se a bomba estiver desligada...
+        if (valorValvulaSubida == "1"){      // e a eletroválvula de subida estiver aberta...
+
+            $("#bomba").attr("fill", "#00FF21");  //Muda cor da bomba
+            $("#botaoBomba").removeClass('btn-danger').addClass('btn-success');
+            $("#botaoBomba").text("Ligada");
+            $("#aguaBomba").attr("stroke","#5B9BA2");
+
+            //Envia requisição para a placa ligar a bomba
+            $.get("http://192.168.0.109/BOMBA=ON");
+
+
+            $("#bomba").attr("value", "1");   //Atualiza o estado da bomba
+
+        } else {
+            window.alert("Abra a válvula antes de ligar a bomba");      //Aviso para evitar forçar a bomba
+        }
+            
+    }
+
+    if (valorAtual == "1") {     //Se a bomba estiver ligada...
+
+        //Muda a cor, envia requisição para a placa e atualiza o estado atual da bomba
+        $("#bomba").attr("fill", "red");
+        $("#bomba").attr("value", "0");
+        $("#botaoBomba").removeClass('btn-success').addClass('btn-danger');
+        $("#botaoBomba").text("Desligada");
+        $("#aguaBomba").attr("stroke","#7f3f00");
+
+        $.get("http://192.168.0.109/BOMBA=OFF");
+    }
+}
+
+//FECHAMENTO DA ELETROVÁLVULA DE SUBIDA E DESLIGAMENTO DA BOMBA
+function paradaTotalSubida() {
+
+    //Muda a cor da bomba e eletroválvula para vermelho
+    $("#bomba").attr("fill", "red");
+    $("#botaoBomba").removeClass('btn-success').addClass('btn-danger');
+    $("#botaoBomba").text("Desligada");
+    $("#luz1").attr("fill", "red");
+    $("#botaoValSub").removeClass('btn-success').addClass('btn-danger');
+    $("#botaoValSub").text("Fechada");
+    $("#aguaBomba").attr("stroke","#7f3f00");
+
+    //Envia requisição para a placa fechar a bomba e desligar eletroválvula
+    $.get("http://192.168.0.109/BOMBA=OFF");    
+    $.get("http://192.168.0.109/LED1=OFF");
+
+    //Altera estados da bomba e eletroválvula
+    $("#bomba").attr("value", "0");
+    $("#eletrovalvula_bomba").attr("value", "0");
+}
+
+//DEFINE POSIÇÃO DOS SENSORES DE NÍVEL (de acordo com a altura e posição y do recipiente)
+
+function setSensores() {
+    cisterna = $("#cisterna");     //Elemento html svg da cisterna
+    poço = $("#poço");     //Elemento html svg do poço
+
+    for (var i = 1; i <= 7; i++) {         //Repetição para cada sensor (começando de baixo para cima)
+
+        sensor = $("#sensor" + String(i));     //Elemento html svg do sensor especifico
+        rect = $("#rect" + String(i));         //Elemento html svg do retangulo do sensor
+        ypoço = parseFloat(poço.attr("y"));                 //Posição y do svg do poço
+        ycisterna = parseFloat(cisterna.attr("y"));         //Posição y do svg da cisterna
+        alturaPoço = parseFloat(poço.attr("height"));       //Altura do poço
+        alturaCisterna = parseFloat(cisterna.attr("height"));      //Altura do poço
+        yRect = parseFloat(rect.attr("y"));                 //Posição y do retangulo do sensor
+
+        switch (i) {            //Decisões de altura especifica para cada sensor
+            case 1: {
+                sensor.attr("transform", "translate(0 " + String(ypoço + (alturaPoço*0.8) - yRect) + ")");
+                break;
+            }
+            case 2: {
+                sensor.attr("transform", "translate(0 " + String(ypoço - yRect) + ")");
+                break;
+            }
+            case 3: {
+                sensor.attr("transform", "translate(0 " + String(ycisterna + (alturaCisterna*0.8) - yRect) + ")");
+                break;
+            }
+            case 4: {
+                sensor.attr("transform", "translate(0 " + String(ycisterna + (alturaCisterna*0.6) - yRect) + ")");
+                break;
+            }
+            case 5: {
+                sensor.attr("transform", "translate(0 " + String(ycisterna + (alturaCisterna*0.4) - yRect) + ")");
+                break;
+            }
+            case 6: {
+                sensor.attr("transform", "translate(0 " + String(ycisterna + (alturaCisterna*0.2) - yRect) + ")");
+                break;
+            }
+            case 7: {
+                sensor.attr("transform", "translate(0 " + String(ycisterna - yRect) + ")");
+                break;
+            }
+            default:
+                break;         
+        }
+    }
+}
+
+//ATUALIZAÇÃO DE VOLUMES DOS RECIPIENTES
+function atualizarVolumes(litros) { //(argumento litros recebido da função de leitura do sensor de fluxo)
+
+    nivelCisterna += litros; //Incrementa o volume da cisterna
+    porcentagemCisterna = Math.round(nivelCisterna / 50 * 100); //Calcula a porcentagem com relação à capacidade da cisterna
+
+    nivelPoço -= litros;    //Diminui o volume do poço
+    porcentagemPoço = Math.round(nivelPoço / 50 * 100); //Calcula a porcentagem com relação à capacidade do poço
+
+    //Escreve/insere nivel e porcentagem da cisterna no html
+    $("#porcentCisterna").text(String(porcentagemCisterna) + " %");
+    $("#nivelCisterna").text("(~ " + String(nivelCisterna.toFixed(2)) + " litros)");
+
+    $("#porcentPoco").text(String(porcentagemPoço) + " %");
+    $("#nivelPoco").text("(~ " + String(nivelPoço.toFixed(2)) + " litros)");
+
+    //Anima água dos recipientes
+    $("#agua").attr("height", String((100 - porcentagemCisterna) / 100 * 240.54123)); //Água da cisterna
+    $("#agua2").attr("height", String((100 - porcentagemPoço) / 100 * 249.19729)); //Água do poço
+    
+    //Atualiza sensores de nível
+    atualizarSensoresNível();
+
+    if ((nivelCisterna >= 50.0 && porcentagemCisterna >= 100) || (nivelPoço <= 0 && porcentagemPoço <= 0)) {    //Se a cisterna estiver cheia ou o poço estiver vazio
+
+        //Fecha eletroválvula de subida e desliga a bomba
+        paradaTotalSubida();
+    }
+
+}
---
+++ Interface com MySQL e php/Versão 3 (atual)/prototipo_interface/prototipo_interface.ino
@@ -0,0 +1,253 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "Trojan";
+const char* senha = "86112064KK";
+
+//dados do site que vai receber a requisição GET
+const char http_site[] = "http://esptest.local/registra-estados.php";
+const int http_port = 80;
+
+IPAddress ip(192,168,0,108);
+WiFiServer server(80);
+WiFiClient client;
+
+int led1 = 0; //d3
+int led2 = 2; //d4
+int potenciometro = A0;
+int bomba = 0;
+int sensCisterna = 0;
+int sensPoco = 100;
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  pinMode(potenciometro, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  digitalWrite(led1, 0);
+  digitalWrite(led2, 0);
+
+  Serial.println();
+  Serial.print("Conectando à rede...");
+
+  WiFi.begin(ssid, senha);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Conectado à rede "); Serial.println(ssid);
+
+  server.begin();
+  Serial.println("Servidor inicializado!");
+
+  Serial.print("URL de conexão: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+}
+
+void loop() {
+  client = server.available();
+  if (!client) {
+    delay(500);
+    Serial.println("NodeMCU - Gravando dados no BD via GET");
+    int bytes = analogRead(A0);
+    float voltagem = bytes * 3.3 / 1023.0;
+    float vazao = voltagem * 30.0 / 3.3;
+
+    int estadoEletroBomba = digitalRead(led1);
+    int estadoEletroDreno = digitalRead(led2);
+    
+    Serial.println("Gravando dados no BD: ");
+    Serial.print(String(vazao)); Serial.println(" l/min");
+    Serial.println(estadoEletroBomba);
+    Serial.println(estadoEletroDreno);
+    Serial.println(bomba);
+    
+    // Envio dos dados do sensor para o servidor via GET
+    if ( !getPage((float)vazao, (int)estadoEletroBomba, (int)estadoEletroDreno, (int)bomba, (int)sensCisterna, (int)sensPoco)) {
+      Serial.println("Falha na requisição GET");
+    }
+    if ( sensCisterna == 100 || sensCisterna == 0) {
+      if ( !getPage2((int)sensCisterna) ) {
+        Serial.println("Falha na requisição GET");
+      }
+    }
+    if ( sensPoco == 100 || sensPoco == 0) {
+      if ( !getPage3((int)sensPoco) ) {
+        Serial.println("Falha na requisição GET");
+      }
+    }
+    return;
+  }
+
+  Serial.println("Novo cliente");
+
+  while (!client.available()) {
+    delay(1);
+  }
+  
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+
+  int valor = LOW;
+
+  if (request.indexOf("/LED1=ON") != -1) {
+    digitalWrite(led1, 1);
+    valor = HIGH;
+  }
+
+  if (request.indexOf("/LED1=OFF") != -1) {
+    digitalWrite(led1, 0);
+    valor = LOW;
+  }
+
+  if (request.indexOf("/LED2=ON") != -1) {
+    digitalWrite(led2, 1);
+    valor = HIGH;
+  }
+
+  if (request.indexOf("/LED2=OFF") != -1) {
+    digitalWrite(led2, 0);
+    valor = LOW;
+  }
+
+  if (request.indexOf("/BOMBA=ON") != -1) {
+    bomba = 1;
+    if ( !getPage4((int)bomba) ) {
+        Serial.println("Falha na requisição GET");
+    }
+  }
+  if (request.indexOf("/BOMBA=OFF") != -1) {
+    bomba = 0;
+    if ( !getPage4((int)bomba) ) {
+        Serial.println("Falha na requisição GET");
+    }
+  }
+
+  for (int i = 7; i > 0; i--) {
+    if (request.indexOf("/SENS" + String(i) + "=ON") != -1) {
+
+      if (i > 2){
+        sensCisterna = (i-2) * 20;
+        i = 3; 
+      } else {
+        sensPoco = (i == 2 ? 100 : 20);
+      }
+      
+    }
+    if (request.indexOf("/SENS" + String(i) + "=OFF") != -1) {
+      if (i > 2){
+        sensCisterna = (i - 3) * 20;
+      } else {
+        sensPoco = (i == 2) ? 20 : 0;
+      }
+    }  
+  }
+
+
+
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("");
+
+  delay(1);
+  Serial.println("Client disconnected");
+  Serial.println("");
+  
+  
+}
+
+// Executa o HTTP GET request no site remoto
+bool getPage(float vazao, int estadoEletroBomba, int estadoEletroDreno, int bomba, int sensCisterna, int sensPoco) {
+  if ( !client.connect(ip, http_port) ) {
+    Serial.println("Falha na conexao com o site ");
+    return false;
+  }
+  String param = "?vazao=" + String(vazao) + "&" + "eletroBomba=" + String(estadoEletroBomba) + "&" + "eletroDreno=" + String(estadoEletroDreno) + "&" + "bomba=" + String(bomba) + "&" + "sensCisterna=" + String(sensCisterna) + "&" + "sensPoco=" + String(sensPoco); //Parâmetros com as leituras
+  Serial.println(param);
+  client.println("GET /registra-estados.php" + param);
+  client.println("Host: ");
+  client.println(http_site);
+  client.println("Connection: close");
+  client.println();
+  client.println();
+
+    // Informações de retorno do servidor para debug
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  return true;
+}
+
+bool getPage2(int sensCisterna) {
+  if ( !client.connect(ip, http_port) ) {
+    Serial.println("Falha na conexao com o site ");
+    return false;
+  }
+  String param = "?nivelCisterna=" + String(sensCisterna); //Parâmetro com as leituras
+  Serial.println(param);
+  client.println("GET /registra-historico-niveis.php" + param);
+  client.println("Host: ");
+  client.println(http_site);
+  client.println("Connection: close");
+  client.println();
+  client.println();
+
+    // Informações de retorno do servidor para debug
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  return true;
+}
+
+bool getPage3(int sensPoco) {
+  if ( !client.connect(ip, http_port) ) {
+    Serial.println("Falha na conexao com o site ");
+    return false;
+  }
+  String param = "?nivelPoco=" + String(sensPoco); //Parâmetro com as leituras
+  Serial.println(param);
+  client.println("GET /registra-historico-niveis.php" + param);
+  client.println("Host: ");
+  client.println(http_site);
+  client.println("Connection: close");
+  client.println();
+  client.println();
+
+    // Informações de retorno do servidor para debug
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  return true;
+}
+
+bool getPage4(int bomba) {
+  if ( !client.connect(ip, http_port) ) {
+    Serial.println("Falha na conexao com o site ");
+    return false;
+  }
+  String param = "?bomba=" + String(bomba); //Parâmetro com as leituras
+  Serial.println(param);
+  client.println("GET /registra-historico-bomba.php" + param);
+  client.println("Host: ");
+  client.println(http_site);
+  client.println("Connection: close");
+  client.println();
+  client.println();
+
+    // Informações de retorno do servidor para debug
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  return true;
+}
